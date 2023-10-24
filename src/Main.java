23123f2458271c5d5d50d9e68a8b97b6a0536816
g21turbo <dg21stew@gmail.com>
2023-10-23 20:34:29 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // The value we will test in the switch statement
+        int switchValue = 1;
+
+        switch (switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+            case 2:
+        }
+
+
+
+    }
+}
